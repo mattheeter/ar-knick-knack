4bdccaf089673458327b26898cd142c516f5a4cd
Matt Heeter <matthewheeter1611@gmail.com>
2026-03-10 22:36:27 -0400

    Box opens based on proximity to camera

--- Assets/Scripts/RingBox.cs
+++ Assets/Scripts/RingBox.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class RingBox : MonoBehaviour
 {
     public Transform lid;
+    public Transform cube;
+    public Transform cam;
     public float xAngle = -120;
     public float yAngle = 50;
     public float zAngle = -45;
@@ -10,25 +12,21 @@ public class RingBox : MonoBehaviour
     public float xLoc = 0.02383169f;
     public float yLoc = 0.02849094f;
     public float zLoc = -0.02633353f;
-
-    public bool open = true;
-
-    void Start()
-    {
-        Debug.Log("Start RingBox");
-    }
+    public bool open = false;
 
     void Update()
     {
-        if (open)
+        // If the cube is moved close to the camera, open the box
+        float distance = Vector3.Distance(lid.position, cam.position);
+        Debug.Log(distance);
+        
+        if (distance < 5)
         {
-            Debug.Log("open");
             lid.localRotation = Quaternion.Euler(xAngle, yAngle, zAngle);
             lid.localPosition = new Vector3(xLoc, yLoc, zLoc);
         }
         else
         {
-            Debug.Log("closed");
             lid.localRotation = Quaternion.Euler(0, -22.5f, 0);
             lid.localPosition = new Vector3(0.02170832f, 0.007613274f, 4.656613e-10f);
         }
